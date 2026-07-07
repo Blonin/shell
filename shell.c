2f223c46d5e3a8751154aad07f169ad11802f4a1
Blonin <gymynus@gmail.com>
2026-07-07 17:28:27 +0200

    ajout des appellation des fonction avant la boucle main

--- shell.c
+++ shell.c
@@ -8,6 +8,12 @@
 //
 // Ce Shell n'a pour le moment pas beaucoup de fonction implémenté 
 
+// déclaration des fonctions
+char *shell_read_line(void);
+char **shell_split_line(char *line);
+int shell_execute(char **args);
+void shell_loop(void);
+int shell_launch(char **args);
 
 // déclaration des includes
 #include <sys/wait.h> // waitpid, WUNTRACED, WIFEXITED, WIFSIGNALED
